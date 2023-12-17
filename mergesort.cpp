08df8710c6f09ff5a934ace4c67d042c9a7130a4
Nikita Babbar <115877450+NIKITA320495@users.noreply.github.com>
2023-12-17 12:40:57 +0530

    Add files via upload
    Merge Sort is a divide-and-conquer sorting algorithm that divides an array into two halves, recursively sorts each half, and then merges the sorted halves to produce a fully sorted array.

---
+++ mergesort.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+void Merge(int arr[],int l,int mid,int r)
+{
+    int n1=mid-l+1;
+    int n2=r-mid;
+    int a[n1],b[n2];
+    for(int i=0;i<n1;i++)
+    {
+        a[i]=arr[l+i];
+    }
+    for(int i=0;i<n2;i++)
+    {
+        b[i]=arr[mid+1+i];
+    }
+    int i=0;
+    int j=0;
+    int k=l;
+    while(i<n1 && j<n2)
+    {
+        if(a[i]<b[j])
+        {
+            arr[k]=a[i];
+            i++;
+            k++;
+        }
+        else{
+            arr[k]=b[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<n1)
+    {
+        arr[k]=a[i];
+            i++;
+            k++;
+    }
+    while(j<n2)
+    {
+        arr[k]=b[j];
+        j++;
+        k++;
+    }
+}
+void mergesort(int arr[],int l,int r)
+{
+    if(l<r)
+    {
+        int mid=(l+r)/2;
+        mergesort(arr,l,mid);
+        mergesort(arr,mid+1,r);
+        Merge(arr,l,mid,r);
+    }
+}
+int main()
+{
+    int arr[5]={5,4,8,2,1};
+    mergesort(arr,0,4);
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i];
+    }
+}
